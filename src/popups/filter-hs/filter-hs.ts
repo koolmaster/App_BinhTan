3dcd2b55c6c73b0ec0707c3783125bc016d36843
koolmaster <koolmaster1408@gmail.com>
2019-06-24 11:14:53 +0700

    complete list hs cua toi

--- src/popups/filter-hs/filter-hs.ts
+++ src/popups/filter-hs/filter-hs.ts
@@ -1,25 +1,44 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the FilterHsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-filter-hs',
   templateUrl: 'filter-hs.html',
 })
 export class FilterHsPage {
-
+  tinhtrang = 0;
+  txtHoSo = '';
+  placeholder = 'Tên';
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FilterHsPage');
+  }
+
+  changeValue(value) {
+    switch (value) {
+      case 1:
+        this.placeholder = 'hồ sơ chưa nộp';
+        break;
+
+      case 2:
+        this.placeholder = 'hồ sơ từ chối giải quyết';
+        break;
+      
+      case 3:
+        this.placeholder = 'hồ sơ chờ giải quyết';
+        break;
+      
+      case 4:
+        this.placeholder = 'hồ sơ đã có kết quả';
+        break;
+
+      default:
+        this.placeholder = 'Tên';
+        break;
+    }
   }
 
 }
